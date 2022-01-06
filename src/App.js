58e8bdf08fb20a91c2b815698830168ccc303c15
ValeraFabrice <hobyraobera@gmail.com>
2022-01-06 17:12:03 +0100

    modif index

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from "react";
-import { BrowserRouter, Router, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import Citadine from "./components/Citadine";
-import Compact from "./components/Compact";
-import Suv from "./components/Suv";
-import About from "./components/About";
-
 
 const App = () => {
   
   return (
     <div className="mainPage">
-    <BrowserRouter>
-      <Header />
-      <Router>
-      <Route path = "/" exact component = {App} />
-        <Route path = "/citadine" component = {Citadine} />
-        <Route path = "/compact" component = {Compact} />
-        <Route path = "/suv" component = {Suv} />
-        <Route path = "/about" component = {About} />
-      </Router>
-      </BrowserRouter>
+    <Header />
+    
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Citadine from "./components/Citadine";
+import Compact from "./components/Compact";
+import Suv from "./components/Suv";
+import About from "./components/About";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>      
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="citadine/" element={<Citadine />} />
+        <Route path="compact/" element={<Compact />} />
+        <Route path="suv/" element={<Suv />} />
+        <Route path="about/" element={<About />} />
+      </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
