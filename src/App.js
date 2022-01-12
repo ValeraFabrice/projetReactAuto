2002d0e543a64a19f134e8eeef4bc9ca0a14b6b9
ValeraFabrice <hobyraobera@gmail.com>
2022-01-12 16:39:48 +0100

    add slide

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import ImageSlider from "./components/ImageSlider"
+import { SliderData } from "./components/SliderData";
+
 
 const App = () => {
   
   return (
     <div className="mainPage">
     <Header />
+    <ImageSlider slides={SliderData} />
     
     </div>
   );
 };
+
 
 export default App;
---
+++ src/components/SliderData.js
@@ -0,0 +1,11 @@
+export const SliderData = [
+    {
+        url:'https://cdn.motor1.com/images/mgl/6JKVk/s3/kleinwagen-2019-die-besten-neuen-city-flitzer.jpg'
+    },
+    {
+        url:'https://www.annonces-automobile.com/images/data/actualite/main/111066.jpg'
+    },
+    {
+        url:'https://images.caradisiac.com/images/5/4/5/4/165454/S1-les-meilleurs-suv-d-occasion-par-budget-de-6-000-eur-a-30-000-eur-541477.jpg'
+    },
+]
