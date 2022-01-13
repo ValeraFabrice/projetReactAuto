2218a04c97797313716468d70ec615bc929292a3
ValeraFabrice <hobyraobera@gmail.com>
2022-01-14 00:39:52 +0100

    add about

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ const App = () => {
       <h3 className="title2">Trouvez votre nouvelle voiture d'occasion chez nous. Pas satisfait, échangez!</h3>
     </main>
     <ImageSlider slides={SliderData} />
-    
+    <section>
+      <h2>Qui sommes-nous?</h2>
+      <p>En créant AUTOSHOP, Nous avons tout simplement inventé une nouvelle solution pour changer de voiture, </p> <br/>
+      <p>Comment ? En vous offrant la possibilité d’acheter en ligne, au téléphone, ou en agence, au meilleur prix et en toute transparence, une voiture neuve ou une voiture d’occasion reconditionnée parmi des dizaines de marques et des centaines de modèles ! </p> <br/>
+      <p>L’ambition : apporter la meilleure réponse au consommateur qui change de voiture, l'offre la plus large, des prix justes, un niveau de garantie et de service imbattable, et des solutions de consommation innovantes.</p>
+    </section>
+
     <Footer />
 
     </div>
